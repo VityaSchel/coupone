b5590be480e3f2ebf446644fcb80bd7fb98bf611
hloth <gitlab@hloth.dev>
2023-05-30 22:48:51 +0400

    Unsubscribe page form API

--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -1,8 +1,13 @@
 import styles from './styles.module.scss'
 import cx from 'classnames'
 
-export function Input({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+export function Input({ className, variant = 'default', ...props }: React.PropsWithChildren<{
+  variant?: 'default' | 'cornered'
+}> & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <input className={cx(styles.input, className)} {...props} />
+    <input className={cx(styles.input, {
+      [styles.default]: variant === 'default',
+      [styles.cornered]: variant === 'cornered'
+    }, className)} {...props} />
   )
 }
--- src/widgets/subscription-page/unsubscribe-form/index.tsx
+++ src/widgets/subscription-page/unsubscribe-form/index.tsx
@@ -1,17 +1,91 @@
-import { CardOutlined } from '@/shared/ui/card-outlined'
+import { Input } from '@/shared/ui/input'
 import styles from './styles.module.scss'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { Button } from '@/shared/ui/button'
+import HelpImage from '@/assets/subscription-page/help.svg'
 
 export function SubscriptionPageUnsubsribeForm() {
   return (
     <div className={styles.unsubscribeForm}>
-      <CardOutlined
-        title={['Как это так', 'без подтверждения по смс?']}
-        className={styles.unsubscribeFormCard}
-      >
-        Мы используем рекуррентные платежи, также известные как рекарринговые платежи или «автоплатежи» — это возможность выполнять регулярные списания денег с банковской карты покупателя или электронного кошелька без повторного ввода реквизитов карты и без участия плательщика для инициации очередного платежа.
-        {'\n\n'}
-        Рекарринговые платежи используют так же такие компании как Яндекс, Google, Amazon, IVI, Netflix и многие другие.
-      </CardOutlined>
+      <Form />
+      <HelpImage />
     </div>
+  )
+}
+
+function Form() {
+  return (
+    <Formik
+      initialValues={{ first6digits: '', last4digits: '' }}
+      validationSchema={
+        Yup.object({
+          first6digits: Yup.string()
+            .matches(/^[0-9]+$/, 'Только цифры')
+            .required('Заполните это поле'),
+          last4digits: Yup.string()
+            .matches(/^[0-9]+$/, 'Только цифры')
+            .required('Заполните это поле')
+        })
+      }
+      onSubmit={(values, { setSubmitting }) => {
+        fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/subscriptions/unsubscribe')
+          .then((response) => alert(response.status === 200 ? 'Вы успешно отписались!' : 'Ошибка!'))
+          .catch(() => alert('Ошибка!'))
+          .finally(() => setSubmitting(false))
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit} className={styles.form}>  
+          <div className={styles.headline}><span>Введите ваши</span> данные для отписки</div>
+          <div className={styles.stacked}>
+            <Input
+              variant='cornered'
+              type='number'
+              placeholder='Первые 6 цифр'
+              name='first6digits'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.first6digits}
+              min={'1'.repeat(6)}
+              max={'9'.repeat(6)}
+            />
+            <span className={styles.error}>{errors.first6digits && touched.first6digits && errors.first6digits}</span>
+          </div>
+          <div className={styles.stacked}>
+            <Input
+              variant='cornered'
+              type='number'
+              placeholder='Последние 4 цифры'
+              name='last4digits'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.last4digits}
+              min={'1'.repeat(4)}
+              max={'9'.repeat(4)}
+            />
+            <span className={styles.error}>{errors.last4digits && touched.last4digits && errors.last4digits}</span>
+          </div>
+          <div>
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className={styles.button}
+            >
+              Отписаться
+            </Button>
+          </div>
+        </form>
+      )}
+    </Formik>
   )
 }
